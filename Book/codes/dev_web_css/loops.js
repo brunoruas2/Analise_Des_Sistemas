e3562acafa7997b0812cfa31f16e5f2aaf21f708
Bruno Ruas <bruno.ruas2@gmail.com>
2022-05-10 18:40:33 -0400

    webdev frontend JS loops and control flow (done)

--- Book/codes/dev_web_css/loops.js
+++ Book/codes/dev_web_css/loops.js
@@ -1,7 +1,14 @@
 // Exemplo de loop com while
-var i = 0
+var i = 6
 
 while (i <= 5) {
     console.log(`Contagem: ${i}`)
     i++
+}
+
+// Exemplo de loop com for
+let frutas = ['pera','uva','maca','banana']
+
+for (let index = 0; index < frutas.length; index++) {
+    console.log(frutas[index])
 }
