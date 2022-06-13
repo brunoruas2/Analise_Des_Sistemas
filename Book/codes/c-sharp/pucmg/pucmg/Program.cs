537563443eb6f5bf9436d61f33d47af85b2f7396
Bruno Ruas <bruno.ruas2@gmail.com>
2022-06-13 09:12:56 -0400

    algoritmo e estruturas de dados doing

--- Book/codes/c-sharp/pucmg/pucmg/Program.cs
+++ Book/codes/c-sharp/pucmg/pucmg/Program.cs
@@ -1,10 +1,6 @@
-﻿// Criaremos um vetor de números inteiros com 5 posições e ler cada um de seus valores.
-// Criaremos duas funções que, tomando esse vetor como parâmetro, irão retornar a soma dos
-// números pares contidos no vetor e a quantidade de números ímpares que ele possui. 
-
-int[] vetor = new int[5];
+﻿int[] vetor = new int[5];
 int somaPar = 0;
-int somaInpar = 0;
+int contaImpar = 0;
 int input;
 
 // Soma de dois valores
@@ -16,18 +12,20 @@ int soma(int valor1, int valor2)
 // Iteracao para lancamento dos valores e somas
 for (int i = 0; i < 5; i++)
 {
-    Console.WriteLine($"Valor da Posição nº: {i + 1}");
+    Console.WriteLine($" Valor da Posição nº: {i + 1}");
     input = int.Parse(Console.ReadLine());
 
     vetor[i] = input;
     if (input % 2 == 0)
     {
         somaPar = soma(somaPar, input);
-    } else
+    }
+    else
     {
-        somaInpar = soma(somaInpar, input);
+        contaImpar += 1;
     }
 };
 
-Console.WriteLine($"SomaPar = {somaPar}, SomaInpar = {somaInpar}");
-Console.WriteLine($"Vetor = ({vetor[0]},{vetor[1]},{vetor[2]},{vetor[3]},{vetor[4]})");
+Console.WriteLine($" SomaPar = {somaPar}, ContaImpar = {contaImpar}");
+
+Console.WriteLine($" Vetor = ({vetor[0]},{vetor[1]},{vetor[2]},{ vetor[3]},{vetor[4]})");
